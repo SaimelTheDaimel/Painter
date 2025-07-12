65639d8054f7a345e66af30fc5643443c1ea5ce8
SaimelTheDaimel <simon.s.stahl@googlemail.com>
2025-07-12 21:08:26 +0200

    Converted rgplat to a reusable scene

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@onready var path_2d: Path2D = $Path2D
+@onready var rg_plat_1: Path2D = $red_grey_platforms/RGPlat1
+@onready var rg_plat_2: Path2D = $red_grey_platforms/RGPlat2
 
 func _ready() -> void:
-	Signals.player_moves.connect(path_2d.move_block)
+	Signals.player_moves.connect(rg_plat_1.move_block)
+	Signals.player_moves.connect(rg_plat_2.move_block)
--- scripts/red_grey_platform.gd
+++ scripts/red_grey_platform.gd
@@ -1,6 +1,7 @@
 extends Path2D
 
 @onready var path_follow: PathFollow2D = $PathFollow2D
+@export var ratio: PathFollow2D
 
 func move_block(dir, delta):
-	path_follow.progress_ratio += delta * dir
+	ratio.progress_ratio += delta * dir
